59e3594d0c5bdc6fe1d70005a66c0581ab2838ae
wangzelong <zelong.wang@orderplus.com>
2020-04-30 21:12:29 +0800

    #。。。

--- whaa-juc-lock/src/main/java/com/whaa/hashmap/HashMapTest.java
+++ whaa-juc-lock/src/main/java/com/whaa/hashmap/HashMapTest.java
@@ -1,5 +1,6 @@
 package com.whaa.hashmap;
 
+import java.util.Collections;
 import java.util.HashMap;
 
 /**
@@ -16,8 +17,11 @@ public class HashMapTest {
         //根据key的hash值 与数组的长度取模得到数组的下标
         //当key的hash值产生hash冲突的时候就产生链表
         //链表长度>=8的时候就转换成红黑树
+        //加载因子0.75采用泊松分布算法 公式(exp(-0.5) * pow(0.5, k)
         String oldValue = hashMap.put("1", "3");
         System.out.println(oldValue);
         System.out.println("李杰".hashCode());
+
+
     }
 }
